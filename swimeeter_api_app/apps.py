6bb73a22c196ff101461b8197f116a81237ec6e4
Bennett Lindberg <bennettlindberg@gmail.com>
2023-07-01 21:38:04 -0500

    create project infrastructure

---
+++ swimeeter_api_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SwimeeterApiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'swimeeter_api_app'
---
+++ swimeeter_auth_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SwimeeterAuthAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'swimeeter_auth_app'
